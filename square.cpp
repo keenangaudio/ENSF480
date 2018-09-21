2c026074ff2099bab49fd7eabe984a1173c99cac
Keenan Gaudio <keenangaudio@me.com>
2018-09-21 15:57:10 -0600

    final by kDawg for today

--- square.cpp
+++ square.cpp
@@ -1,11 +1,12 @@
 #include "graphics.h"
 
-Square::Square(float side, float x_, float y_, float z_, char* name){
+Square::Square(float side, float x_, float y_, float z_, char* name) {
   side_a = side;
   origin = new Point(x_, y_, z_);
-
+  shapeName = new char[strlen(source.shapeName)+1]
 }
 Square::Square(float side, const Shape& rhs){
+  side_a = side;
 
 }
 float Square::area(void){
