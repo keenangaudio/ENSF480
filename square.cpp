71a3f258d26e06ffdb0cd237ff2f396b1480d8d1
Keenan Gaudio <keenangaudio@me.com>
2018-09-21 15:53:02 -0600

    no clue

--- square.cpp
+++ square.cpp
@@ -1,1 +1,25 @@
 #include "graphics.h"
+
+Square::Square(float side, float x_, float y_, float z_, char* name){
+  side_a = side;
+  origin = new Point(x_, y_, z_);
+
+}
+Square::Square(float side, const Shape& rhs){
+
+}
+float Square::area(void){
+
+}
+float Square::perimeter(void){
+
+}
+float Square::getSideA(void){
+
+}
+void Square::setSideA(void){
+
+}
+void Square::display(void){
+
+}
