3b660f98459de926ce2fb762ff91c8d733e01e51
Keenan Gaudio <keenangaudio@me.com>
2018-09-21 15:08:22 -0600

    formatting

--- rectangle.cpp
+++ rectangle.cpp
@@ -2,18 +2,18 @@
 #include "graphics.h"
 
 
-  float area(void) {
-    return 0;
-  }
-  float perimeter(void) {
-    return 0;
-  }
-  float getSideB(void) {
-    return 0;
-  }
-  void setSideB(void) {
-    return 0;
-  }
-  void display(void) {
-    return 0;
-  }
+float area(void) {
+  return 0;
+}
+float perimeter(void) {
+  return 0;
+}
+float getSideB(void) {
+  return 0;
+}
+void setSideB(void) {
+  return 0;
+}
+void display(void) {
+  return 0;
+}
